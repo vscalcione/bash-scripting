70e1d1441560fe387c55100e3bb708b24a6a867a
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-20 14:37:46 +0200

    Fixing project structure

---
+++ dev-scripts/install-codeblocks-ide.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:damien-moore/codeblocks-stable
+sudo apt update
+sudo apt install codeblocks codeblocks-contrib
---
+++ system-management/update-system.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# If you are on a Debian based Distro, type this on terminal for update your sistem with all packages and then install them
+sudo apt update
+sudo apt upgrade
+
+# Other distros. update your system with all pakages
+sudo apt-get update
+sudo apt-get upgrade
+
+

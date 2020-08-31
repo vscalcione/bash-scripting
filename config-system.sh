c27a3cd28f0c3a0d2d6a4f1820d6a743ded26656
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-08-31 18:02:40 +0200

    Refactoring script

--- config-system.sh
+++ config-system.sh
@@ -72,7 +72,7 @@ INSTALL_DART_SDK(){
 	sudo sh -c "wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -"
 	sudo sh -c "wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list"
 	sudo apt update
-	sudo apt install dart	
+	sudo apt install dart
 }
 
 INSTALL_NGINX_WEBSERVER(){
@@ -82,7 +82,7 @@ INSTALL_NGINX_WEBSERVER(){
 	curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 	sudo apt-key fingerprint ABF5BD827BD9BF62
 	sudo apt update
-	sudo apt install nginx	
+	sudo apt install nginx
 }
 
 INSTALL_MONODEVELOP(){
@@ -91,7 +91,21 @@ INSTALL_MONODEVELOP(){
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 	echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
 	sudo apt update
-	sudo apt install monodevelop	
+	sudo apt install monodevelop
+}
+
+INSTALL_VNC_VIEWER(){
+	echo 'STEP 9: VNC Viewer'
+	wget https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.20.529-Linux-x64.deb
+	sudo dpkg -i VNC-Viewer-6.20.259-Linux-x64.deb
+}
+
+INSTALL_SKD_MAN_GRADLE_GROOVY_CONSOLE(){
+	echo 'STEP 10: SDKMAN, GROOVY'
+	curl -s "https://get.sdkman.io" | bash
+	source "$HOME/.sdkman/bin/sdkman-init.sh"
+	sdk version
+	sdk install gradle groovy
 }
 
 INSTALL_NPM_PACKAGES(){
@@ -106,60 +120,68 @@ INSTALL_NPM_PACKAGES(){
 # Method for installation of all software
 INSTALL_SOFTWARE() {
 	echo $"\nSTEP 2/3: INSTALL SOFTWARE WITH APT"
-	sudo apt-get install audacity
-	sudo apt-get install brasero
-	sudo apt-get install cmatrix
-	sudo apt-get install deepin-terminal
-	sudo apt-get install 
-	sudo apt-get install idle3
-	sudo apt-get install vim
-	sudo apt-get install xterm
-	sudo apt-get install php
-	sudo apt-get install python3
-	sudo apt-get install evolution
-	sudo apt-get install geary
-	sudo apt-get install gdebi
-	sudo apt-get install gimp
-	sudo apt-get install gnome-tweaks
-	sudo apt-get install gparted
-	sudo apt-get install gsmartcontrol
-	sudo apt-get install htop
-	sudo apt-get install handbrake
-	sudo apt-get install htop
-	sudo apt-get install synaptic
-	sudo apt-get install vlc
-	sudo apt-get install meld
-	sudo apt-get install gnome-system-monitor
-	sudo apt-get instal okular
-	sudo apt-get install openjdk-8-jdk openjdk-11-jdk
-	sudo apt-get install putty
-	sudo apt-get install qbittorrent
-	sudo apt-get install remmina
-	sudo apt-get install shotwell
-	sudo apt-get install transmissions
-	sudo apt-get install fish
-	sudo apt-get install virtualbox
-	sudo apt-get install wireshark
-	sudo apt-get install net-tools
-	sudo apt-get install snapd
-	sudo apt-get install mc
-	sudo apt-get install rpi-imager
-	sudo apt-get install vifm
+	sudo apt install audacity
+	sudo apt install brasero
+	sudo apt install cmatrix
+	sudo apt install deepin-terminal
+	sudo apt install deepin-image-viewer
+	sudo apt install evolution
+	sudo apt install filezilla
+	sudo apt install fish
+	sudo apt install geary
+	sudo apt install gdebi
+	sudo apt install gimp
+	sudo apt install gnome-tweaks
+	sudo apt install gparted
+	sudo apt install gsmartcontrol
+	sudo apt install grub-customizer
+	sudo apt install gnome-system-monitor
+	sudo apt install htop
+	sudo apt install handbrake
+	sudo apt install idle3
+	sudo apt install meld
+	sudo apt install mc
+	sudo apt install net-tools
+	sudo apt install okular
+	sudo apt install openjdk-8-jdk openjdk-11-jdk
+	sudo apt install php
+	sudo apt install python3
+	sudo apt install putty
+	sudo apt install qbittorrent
+	sudo apt install rpi-imager
+	sudo apt install remmina
+	sudo apt install synaptic
+	sudo apt install samba
+	sudo apt install shotwell
+	sudo apt install snapd
+	sudo apt install transmissions
+	sudo apt install tree
+	sudo apt install trash-cli
+	sudo apt install vlc
+	sudo apt install virtualbox
+	sudo apt install vim
+	sudo apt install vifm
+	sudo apt install wireshark
+	sudo apt install xterm
 
 	echo $"\nSTEP 3/4: INSTALL SOFTWARE WITH SNAP"
 	sudo snap install bitwarden
-	 
+	sudo snap install snap-store
 
 	echo $'\nAll software installed.\nPackages installed: '
-    echo $'1. audacity\n2. brasero\n3. cmatrix\n4. deepin-terminal\n5. idle3\n4. vim\n5. xterm\n6. php\n7. php\n8. evolution\n9. geary\n10. gdebi'
-	echo $'11. gimp\n12. gnome-tweaks\n13. fish\n14. virtualbox\n15. wireshark\n16. net-tools\n17. snapd'
+	echo $'1. audacity\n2. brasero\n3. cmatrix\n4. deepin-terminal\n5. deepin-image-viewer\n6. evolution\n7. filezilla\n8. fish\n9. geary\n10. gdebi'
+	echo $'11. gimp\n12. gnome-tweaks\n13. gparted\n14. gsmartcontrol\n15. grub-customizer\n16. gnome-system-monitor\n17. htop\n18. handbrake\n19. idle3\n20. meld'
+	echo $'12. mc\n13. net-tools\n14. okular\n15. openjdk-8-jdk openjdk-11-jdk\n16. php\n17. python3\n18.putty\n19. qbittorrent\n20. rpi-imager'
+	echo $'21. remmina\n22. synaptic\n23. samba\n24. shotwell\n25. snapd\n26. transmissions\n27. tree\n28. trash-cli\n29. vlc\n30. virtualbox'
+	echo $'31. vim\n32. vifm\n33. wireshark\n34. xterm' 
 }
 
-UPDATE_AND_UPGRADE_SYSTEM
-INSTALL_SOFTWARE
+
 
 if [1 == 2]
 then
+	UPDATE_AND_UPGRADE_SYSTEM
+	INSTALL_SOFTWARE
 	INSTALL_RABBITVCS
 	INSTALL_GOOGLE_CHROME
 	INSTALL_MONGODB
@@ -170,4 +192,7 @@ then
 	INSTALL_DART_SDK
 	INSTALL_NGINX_WEBSERVER
 	INSTALL_MONODEVELOP
+	INSTALL_VNC_VIEWER
+	INSTALL_SKD_MAN_GRADLE_GROOVY_CONSOLE
+	INSTALL_NPM_PACKAGES
 fi

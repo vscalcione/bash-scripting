13155ac309140c2c4aadcebd67e9c6212875e852
DOMUS\vscalcione <vscalcione@sogei.it>
2024-02-17 00:46:08 +0100

    Updated get-git-url.sh and install-docker.sh scripts

--- dev-scripts/get-git-url.sh
+++ dev-scripts/get-git-url.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-git config --get remote.origin.url
+INPUT_FOLDER="$1"
 
+if [ ! -d "$INPUT_FOLDER/.git" ]; then
+    echo -e "Error: Not a git repository"
+fi
+
+cd "$INPUT_FOLDER" || exit 1
+GIT_REMOTE_URL=$(git config --get remote.origin.url)
+
+echo -e "$GIT_REMOTE_URL"
--- dev-scripts/install-docker.sh
+++ dev-scripts/install-docker.sh
@@ -1,7 +1,12 @@
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
